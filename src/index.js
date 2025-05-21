737047878ebca66da59075b12b23c83f045d0ddb
KalleoLima <kalleo.cavalcante.lima@gmail.com>
2025-05-21 17:50:35 -0300

    desafio do felipao

--- src/index.js
+++ src/index.js
@@ -1,44 +1,38 @@
 let nome = "lekfera"
 let xp = 2001
-let rank;
+let nivel;
 
 if (xp <= 1000) {
-  console.log("ferro")
-  rank = "ferro"
+  nivel = "ferro"
 }
 
 else if (xp >= 1001 && xp <= 2000) {
-  console.log("bronze")
-  rank = "bronze"
+  nivel = "bronze"
 }
 
 else if (xp >= 2001 && xp <= 6000) {
-  console.log("prata")
-  rank = "prata"
+  nivel = "prata"
 }
 
 else if (xp >= 6001 && xp <= 7000) {
-  console.log("ouro")
-  rank = "ouro"
+  nivel = "ouro"
 }
 
 else if (xp >= 7001 && xp <= 8000) {
-  console.log("platina")
-  rank = "platina"
+  nivel = "platina"
 }
 
 else if (xp >= 8001 && xp <= 9000) {
-  console.log("ascendente")
-  rank = "ascendente"
+  nivel = "ascendente"
 }
 
 else if (xp >= 9001 && xp <= 10000) {
-  console.log("imortal")
-  rank = "imortal"
+  nivel = "imortal"
 }
 
 else if (xp > 10000) {
-  console.log("radiante")
-  rank = "radiante"
+  nivel = "radiante"
 }
+
+console.log("o herói do nome " + nome + " esta no nivel de " + nivel)
 
