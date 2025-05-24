ac2d51945e1a290c9f8162ffd7f1bcf9dffc7a88
KalleoLima <kalleo.cavalcante.lima@gmail.com>
2025-05-24 19:46:04 -0300

    projeto do felipaoo

---
+++ src/projeto.js
@@ -0,0 +1,30 @@
+function jogador(vitoria, derrota) {
+  let saldovitorias = vitoria - derrota
+  let rank;
+  if (saldovitorias <= 10) {
+    rank = "ferro"
+  }
+  else if (saldovitorias >= 11 && saldovitorias <= 20) {
+    rank = "bronze"
+  }
+  else if (saldovitorias >= 21 && saldovitorias <= 50) {
+    rank = "prata"
+
+  }
+  else if (saldovitorias >= 51 && saldovitorias <= 80) {
+    rank = "ouro"
+
+  }
+  else if (saldovitorias >= 81 && saldovitorias <= 90) {
+    rank = "diamante"
+  }
+  else if (saldovitorias >= 91 && saldovitorias <= 100) {
+    rank = "lendario"
+  }
+  else if (saldovitorias >= 101) {
+    rank = "imortal"
+  }
+  return (`o saldo de vitorias é ${saldovitorias} e o rank do jogador é ${rank}`)
+}
+const resultado = jogador(20, 10)
+console.log(resultado)
